6758640854c87eb5e8741e65f3b906935875f542
CD Clark III <clifton.clark@gmail.com>
2024-05-07 21:24:50 -0500

    testing: added benchmarks

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,21 @@
+import math
+
+import numpy
+import pytest
+import scipy
+
+
+def test_numpy_exp(benchmark):
+    benchmark(numpy.exp, 0.1)
+
+
+def test_math_exp(benchmark):
+    benchmark(math.exp, 0.1)
+
+
+def test_scipy_erf(benchmark):
+    benchmark(scipy.special.erf, 0.1)
+
+
+def test_math_erf(benchmark):
+    benchmark(math.erf, 0.1)
