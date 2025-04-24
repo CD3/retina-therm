2399aa39c048a953ee95f186ef668968aee6fd73
CD Clark III <clifton.clark@gmail.com>
2025-04-23 21:18:40 -0500

    test: fixed parallel_job test

--- tests/test_parallel_jobs.py
+++ tests/test_parallel_jobs.py
@@ -7,12 +7,6 @@ from retina_therm.parallel_jobs import *
 def test_parallel_job_controller():
     class MyProcess(JobProcess):
         def run_job(self, config):
-            print("run job", os.getpid())
-            self.status.emit("STATUS CHECK 1")
-            self.progress.emit(1, 2)
-            time.sleep(1)
-            time.sleep(1)
-            time.sleep(1)
             time.sleep(1)
 
     controller = Controller(MyProcess, 1, no_progress_display=False)
@@ -23,14 +17,12 @@ def test_parallel_job_controller():
 
     assert end - start > 1
     assert end - start > 2
-    print(controller.results)
-    # assert len(controller.results) == 2
 
-    # controller = Controller(MyProcess, 2, no_progress_display=True)
-    # start = time.perf_counter()
-    # controller.run([1, 2])
-    # end = time.perf_counter()
-    # controller.stop()
+    controller = Controller(MyProcess, 2, no_progress_display=True)
+    start = time.perf_counter()
+    controller.run([1, 2])
+    end = time.perf_counter()
+    controller.stop()
 
-    # assert end - start > 1
-    # assert end - start < 1.5
+    assert end - start > 1
+    assert end - start < 1.5
