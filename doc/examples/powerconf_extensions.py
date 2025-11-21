0c66c8f448fdb2136d83a67966daee8b25e8d0e8
CD Clark III <clifton.clark@gmail.com>
2025-11-21 06:26:02 -0600

    chore: code formatter on example file.

--- doc/examples/powerconf_extensions.py
+++ doc/examples/powerconf_extensions.py
@@ -1,19 +1,26 @@
-from powerconf.utils import get_id
-import yaml
-import numpy
+import logging
 from pathlib import Path
+
+import numpy
+import yaml
 from powerconf.units import Q_
+from powerconf.utils import get_id
+
+logging.basicConfig(filename="powerconf_extensions.log", level=logging.INFO)
 
 
 def get_scale(filename):
     filepath = Path(filename)
+    logging.info(f"getting scale from {filepath}")
     if not filepath.exists():
+        logging.info(f"file does not exist")
         return None
     config = yaml.safe_load(filepath.read_text())
-    return config['scale']
+    return config["scale"]
+
 
 def get_peak_temperature(filename):
     data = numpy.loadtxt(filename)
-    v = max(data[:,1])
+    v = max(data[:, 1])
 
-    return Q_(v,'degC')
+    return Q_(v, "degC")
